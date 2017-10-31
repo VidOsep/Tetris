2d639163c9e9332925ab535c6734be2619a7c6f4
VidOsep <vid.osep@gmail.com>
2017-10-31 20:07:41 +0100

    Update matrix_grid.py

--- matrix_grid.py
+++ matrix_grid.py
@@ -11,7 +11,16 @@ element4 = np.matrix([[0,1],[1,1],[0,1]])
 element_arr = [element1,element2,element3,element4]
 
 while True:
-    tm.sleep(0.8)
+    tm.sleep(1)
     rnd.shuffle(element_arr)
     for i in element_arr:
-        print i
+        if i == element1:
+            grid[0:2,0:3] = i
+        elif i == element2:
+            grid[0:2,0:3] = i
+        elif i == element3:
+            grid[0:2,0:4] = i
+        elif i == element4:
+            grid[0:3,0:3] = i
+        print grid
+        tm.sleep(3)
