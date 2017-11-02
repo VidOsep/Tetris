ca9a8445b45ce5383a32e274fa06537e03ff86dc
VidOsep <vid.osep@gmail.com>
2017-11-02 08:55:25 +0100

    Create element.py

---
+++ element.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Element:
+    element1 = np.matrix([[0, 1], [0, 1], [1, 1], [0, 0]])
+    element2 = np.matrix([[0, 1], [0, 1], [0, 1], [0, 1]])
+    element3 = np.matrix([[1, 1], [1, 1], [0, 0], [0, 0]])
+    element4 = np.matrix([[0, 1], [1, 1], [0, 1], [0, 0]])
+    element_arr = [element1, element2, element3, element4]
