08034c230364a210abb9089c11ee9bafe0dd6d52
VidOsep <vid.osep@gmail.com>
2017-11-05 21:26:58 +0100

    Update matrix_grid.py

--- matrix_grid.py
+++ matrix_grid.py
@@ -10,7 +10,7 @@ grid = np.matrix([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0
 while True:
     tm.sleep(1)
     element_arr = x.element_arr
-    element_arr = rnd.shuffle(element_arr)
+    rnd.shuffle(element_arr)
     for i in element_arr:
         x = rnd.randint(0,4)
         grid[0:4,x:x+2] = i
