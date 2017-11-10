7ded56d4d240966680e7417854812ebf2f17b303
VidOsep <vid.osep@gmail.com>
2017-11-10 21:13:17 +0100

    Update matrix_grid.py

--- matrix_grid.py
+++ matrix_grid.py
@@ -15,10 +15,16 @@ while True:
     rnd.shuffle(ele_arr)
     for i in ele_arr:
         x = rnd.randint(0,4)
-        for y in range(10):
-            grid[y:y+4,x:x+2] = i
-            print grid
-            grid[y:y+1, 0:7] = blank_line
-            tm.sleep(0.7)
-        print grid
+        for y in range(6):
+            if y < 5:
+                grid[y:y+4,x:x+2] = i
+                print grid
+                grid[y:y + 1, 0:7] = blank_line
+                tm.sleep(0.7)
+            else:
+                grid[y:y+4,x:x+2] = i
+                print grid
+                if grid[y+2,x+1] == 1:
+                    grid[y+1:y+4,x:x+2] = grid[y:y+3,x:x+2]
+                tm.sleep(0.7)
         tm.sleep(5)
