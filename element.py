394a120998ba7bb027c20222679f14ddde847f7d
VidOsep <vid.osep@gmail.com>
2017-11-10 21:14:08 +0100

    Update element.py

--- element.py
+++ element.py
@@ -6,3 +6,7 @@ class Element:
     element3 = np.matrix([[1, 1], [1, 1], [0, 0], [0, 0]])
     element4 = np.matrix([[0, 1], [1, 1], [0, 1], [0, 0]])
     element_arr = [element1, element2, element3, element4]
+    def check_if_touching(self,grid,under_el):
+        if any(grid[under_el]):
+            return 3
+
