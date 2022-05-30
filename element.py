e5c65f4d974a2e3d5210ea9d14614c77fa6f1d2e
vidos <vid.osep@gmail.com>
2022-05-30 22:47:04 +0200

    Cleaned up some code

--- element.py
+++ element.py
@@ -3,6 +3,10 @@ import random
 
 
 class Element:
+    """
+    Skrbi za funkcije in obnasanje tetronimov
+    """
+
     def __init__(self, x):
         self.chooseRandom()
 
@@ -10,101 +14,103 @@ class Element:
         self.y = 0
 
     def chooseRandom(self):
+        # izbere nakljucen tetronimo in barvo
         e1 = np.array([[0, 1, 0, 0], [0, 1, 0, 0], [1, 1, 0, 0], [0, 0, 0, 0]], dtype=int)
         e2 = np.array([[1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0]], dtype=int)
         e3 = np.array([[1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], dtype=int)
         e4 = np.array([[0, 1, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0]], dtype=int)
-        e5 = np.array([[0,1,0,0], [0,0,0,1], [1,0,0,0], [0,0,1,0]], dtype=int)
-        e6 = np.array([[0,1,0,0], [0,1,0,0], [1,1,1,0], [0,1,0,0]], dtype=int)
-        e7 = np.array([[1,1,1,0,1], [0,0,1,0,1], [1,1,1,1,1], [1,0,1,0,0],[1,0,1,1,1]], dtype=int)
+        e5 = np.array([[0, 1, 0, 0], [0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0]], dtype=int)
 
-
-        colors = ["LIGHT_GREEN","LIGHT_BLUE","LIGHT_RED","YELLOW","PURPLE"]
-        element_arr = [e1,e2,e3,e4,e5,e6,e7]
+        colors = ["LIGHT_GREEN", "LIGHT_BLUE", "LIGHT_RED", "YELLOW", "PURPLE"]
+        element_arr = [e1, e2, e3, e4, e5]
         self.element = random.choice(element_arr)
         self.color = random.choice(colors)
 
     def getLastRow(self):
-        a,b=0,0
+        # vrne dolzino tetronima
+        a, b = 0, 0
         for i in range(len(self.element[0])):
             if np.sum(self.element[i]) != 0:
-                if a==0:
-                    b=i
-                a+=1
-        return a+b
+                if a == 0:
+                    b = i
+                a += 1
+        return a + b
 
     def getLastCol(self):
-        a,b=0,0
+        # vrne visino tetronima
+        a, b = 0, 0
         for i in range(len(self.element)):
-            if np.sum(self.element[:,i]) != 0:
-                if a==0:
-                    b=i
-                a+=1
-        return a+b
+            if np.sum(self.element[:, i]) != 0:
+                if a == 0:
+                    b = i
+                a += 1
+        return a + b
 
-    def isColliding(self,x,y,grid):
+    def isColliding(self, x, y, grid):
         # preveri ce se tetronim na poziciji prekriva z igralno povrsino
-        new = np.zeros((20,10),dtype=int)
+        new = np.zeros((20, 10), dtype=int)
         iy = self.getLastRow()
         ix = self.getLastCol()
         try:
-            new[y:y+iy,x:x+ix] = self.element[0:iy,0:ix]
+            new[y:y + iy, x:x + ix] = self.element[0:iy, 0:ix]
         except:
             return True
-        overlap = np.bitwise_and(new,grid)
+        overlap = np.bitwise_and(new, grid)
         if np.sum(overlap) > 0:
             return True
         return False
 
     def shiftElArrayLeft(self):
         # zamakne obliko tetronima v levo
-        self.element = self.element[:,1:]
-        new_column = np.zeros((len(self.element),1))
+        self.element = self.element[:, 1:]
+        new_column = np.zeros((len(self.element), 1))
         self.element = np.append(self.element, new_column, axis=1)
 
     def moveToLeftUp(self):
         # prestavi tetronim v zgornji levi kot matrike
-        while np.sum(self.element[:,0]) == 0:
+        while np.sum(self.element[:, 0]) == 0:
             self.shiftElArrayLeft()
 
-    def moveDown(self,grid):
+    def moveDown(self, grid):
         # premakne tetronimo navzdol
-        if self.y+self.getLastRow()>19 or self.isColliding(self.x,self.y+1,grid):
+        if self.y + self.getLastRow() > 19 or self.isColliding(self.x, self.y + 1, grid):
             return False
         else:
             self.y += 1
             return True
 
-    def moveRight(self,grid):
+    def moveRight(self, grid):
         # premakne tetronimo v desno
-        if self.x+self.getLastCol()>=18 or self.isColliding(self.x+1,self.y,grid):
+        if self.x + self.getLastCol() >= 18 or self.isColliding(self.x + 1, self.y, grid):
             return False
         else:
             self.x += 1
             return True
 
-    def moveLeft(self,grid):
+    def moveLeft(self, grid):
         # premakne tetronimo v levo
-        if self.x<1 or self.isColliding(self.x-1,self.y,grid):
+        if self.x < 1 or self.isColliding(self.x - 1, self.y, grid):
             return False
         else:
             self.x -= 1
             return True
 
-    def rotateRight(self,grid):
+    def rotateRight(self, grid):
         # rotacija za 90 stopinj
         old = self.element
         self.element = np.rot90(self.element)
         self.moveToLeftUp()
         try:
-            if not self.isColliding(self.x,self.y,grid):
+            if not self.isColliding(self.x, self.y, grid):
                 return True
             else:
                 self.element = old
                 return False
         except:
             return False
-    def drawColors(self,new,colors):
+
+    def drawColors(self, new, colors):
+        # izrise barve tetronima na igralno povrsino
         for i in range(len(new)):
             for j in range(len(new[0])):
                 if new[i][j]:
@@ -112,11 +118,12 @@ class Element:
         return colors
 
     def draw(self, grid, colors):
-        new = np.zeros((20,10),dtype=int)
+        # izrise obliko tetronima na igralno povrsino
+        new = np.zeros((20, 10), dtype=int)
         iy = self.getLastRow()
         ix = self.getLastCol()
-        new[self.y:self.y+iy,self.x:self.x+ix] = self.element[0:iy,0:ix]
+        new[self.y:self.y + iy, self.x:self.x + ix] = self.element[0:iy, 0:ix]
 
-        colors = self.drawColors(new,colors)
+        colors = self.drawColors(new, colors)
 
-        return np.bitwise_or(new,grid),colors
+        return np.bitwise_or(new, grid), colors
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import pygame as pg
+
 from element import Element
 
 pg.init()
@@ -8,23 +9,24 @@ pg.init()
 DISPLAYSURF = pg.display.set_mode((400, 400))
 pg.display.set_caption('Tetris')
 
+# igralna povrsina sestavljena iz binarne matrike - grid in matrike nizov - colors (oblika in barve)
 grid = np.zeros((20, 10), dtype=int)
-colors = np.empty((20,10),dtype=object)
+colors = np.empty((20, 10), dtype=object)
 
-BG = pg.Color(0, 64, 128)
+BG = pg.Color(0, 64, 128)  # barva ozadja
 
-# za debuggiranje
 el = Element(3)
 
 score = 0
 
-#initialize font
+# fonti
 pg.font.init()
 bigf = pg.font.SysFont('Comic Sans MS', 40)
 smallf = pg.font.SysFont('Comic Sans MS', 20)
 
 
 def drawMatrix(matrix, colors, display):
+    # izrise igralno povrsino na display
     LIGHT_GREEN = (124, 252, 0)
     LIGHT_BLUE = (32, 178, 170)
     LIGHT_RED = (255, 0, 0)
@@ -45,7 +47,6 @@ def drawMatrix(matrix, colors, display):
 
     for i in range(20):
         for j in range(10):
-
             if matrix[i][j] == 1:
                 if colors[i][j] == "LIGHT_GREEN":
                     pg.draw.rect(display, LIGHT_GREEN, (x_pos, y_pos, width, height))
@@ -61,41 +62,49 @@ def drawMatrix(matrix, colors, display):
                     pg.draw.rect(display, red, (x_pos, y_pos, width, height))
             else:
                 pg.draw.rect(display, PLAYAREABG, (x_pos, y_pos, width, height))
-
             x_pos += width + margin
         y_pos += height + margin
         x_pos = start_pos
 
-def title(x, y):
+
+def title(x=0, y=0):
+    # izrise naslov na zaslon
     title_ = bigf.render('TETRIS', True, (255, 255, 255))
     subtitle_ = smallf.render('MADE BY VID', True, (255, 255, 255))
-    
-    title_rect = title_.get_rect(center=(200/2, 50))
-    subtitle_rect = subtitle_.get_rect(center=(200/2, 100))
+
+    title_rect = title_.get_rect(center=(200 / 2, 50))
+    subtitle_rect = subtitle_.get_rect(center=(200 / 2, 100))
 
     DISPLAYSURF.blit(title_, title_rect)
     DISPLAYSURF.blit(subtitle_, subtitle_rect)
 
+
 def scoreBlit(score):
+    # izrise tocke na zaslon
     score_ = smallf.render("Tocke: " + str(score), True, (255, 255, 255))
-    score_rect = score_.get_rect(center=(200/2, 300))
+    score_rect = score_.get_rect(center=(200 / 2, 300))
 
     DISPLAYSURF.blit(score_, score_rect)
 
+
 def checkForLines():
-    global grid
-    global score
+    # preverja za polne linije
+    global grid, colors, score
     for i in range(len(grid)):
-        if np.sum(grid[i])==10:
-            score+=10
-            grid = np.delete(grid,i,axis=0)
-            grid = np.concatenate((np.zeros((1,10),dtype=int),grid))
+        if np.sum(grid[i]) == 10:  # odkrita polna linija
+            score += 10
+            grid = np.delete(grid, i, axis=0)
+            grid = np.concatenate((np.zeros((1, 10), dtype=int), grid))
+
+            colors = np.delete(colors, i, axis=0)
+            colors = np.concatenate((np.empty((1, 10), dtype=object), colors))
 
 
-PREMIKDOL,t = pg.USEREVENT+1,750
-pg.time.set_timer(PREMIKDOL, t)
+MOVEDOWN, t = pg.USEREVENT + 1, 750
+pg.time.set_timer(MOVEDOWN, t)
 
 while True:
+    # pregled vseh eventov
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
@@ -110,44 +119,43 @@ while True:
             elif event.key == pg.K_UP:
                 # zarotiraj trenutni tetronim
                 el.rotateRight(grid)
-            elif event.key == pg.K_a:
-                # zarotiraj trenutni tetronim
-                el.shiftElArrayLeft()
             elif event.key == pg.K_DOWN:
-                if t==750:
-                    t=100
+                # toggle hitrejsi padec
+                if t == 750:
+                    t = 100
                 else:
-                    t=750
-                pg.time.set_timer(PREMIKDOL, t)
-        # lokalno definiran event
-        if event.type == PREMIKDOL:
-            prejsnja = grid
-            prejsnjac = colors
-            grid,colors = el.draw(grid,colors)
-            if not(el.moveDown(prejsnja)):    # tetronimo doseze konec
+                    t = 750
+                pg.time.set_timer(MOVEDOWN, t)
+        # lokalno definiran event - premik dol
+        if event.type == MOVEDOWN:
+            tempgrid = grid
+            tempgridc = colors
+            grid, colors = el.draw(grid, colors)
+            if not (el.moveDown(tempgrid)):  # tetronimo doseze konec
                 # preveri za ustvarjene linije
-
                 checkForLines()
-
+                # ustvari nov element, preveri ce ni starih ze pri vrhu
                 el = Element(3)
-                if el.isColliding(el.x,el.y,grid):
+                if el.isColliding(el.x, el.y, grid):
                     pg.quit()
                     sys.exit()
             else:
-                grid = prejsnja
-                colors = prejsnjac
+                grid = tempgrid
+                colors = tempgridc
 
-    # izris matrike
-    prejsnja = grid
-    prejsnjac = colors
-    grid,colors = el.draw(grid,colors)
+    # izris tetronima na matriko
+    tempgrid = grid
+    tempgridc = colors
+    grid, colors = el.draw(grid, colors)
 
+    # izris besedila
     DISPLAYSURF.fill(BG)
-    title(0,0)
+    title()
     scoreBlit(score)
 
-    drawMatrix(grid,colors, DISPLAYSURF)
-    grid = prejsnja
-    colors = prejsnjac
+    # izris matrike grid na zaslon
+    drawMatrix(grid, colors, DISPLAYSURF)
+    grid = tempgrid
+    colors = tempgridc
 
     pg.display.update()
